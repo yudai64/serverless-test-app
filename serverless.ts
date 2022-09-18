d71ed1c2e42f877b39b000b1890a5c9660f507ae
yudaiyano <kemurin.yudai0723@gmail.com>
2022-09-18 22:09:42 +0900

    コメント

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import hello from "@functions/hello";
 const serverlessConfiguration: AWS = {
   service: "serverless-test-app",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild"],
+  plugins: ["serverless-esbuild"], // 必要なプラグインをここに設定
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
@@ -13,6 +13,7 @@ const serverlessConfiguration: AWS = {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
+    // 利用するすべてのLambdaに設定する環境変数
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
